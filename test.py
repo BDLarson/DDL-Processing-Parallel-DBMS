230c42c40887841f030cf1be49fe7e3232e9f0bb
Evelyn Pirnia <evelynpirnia@gmail.com>
2017-01-31 16:01:15 -1000

    read in driver, parse url, create catalog via sql statement

--- test.py
+++ test.py
@@ -3,12 +3,17 @@ import pymysql
 
 import sys
 from sys import argv
+import re
+
 
 def runDDL(argv):
     # Define variables
     hostname = ""
     username = ""
     passwd = ""
+    url = ""
+    port = ""
+    db = ""
 
     # List of Nodes
     nodes = []
@@ -25,40 +30,58 @@ def runDDL(argv):
                 temp = line.strip().split("=")
                 if temp[0].split(".")[0].find("catalog") > -1:
                     kind = "catalog"
-                    if temp[0].split(".")[1].find("hostname") > -1:
-                        hostname = temp[1]
+                    if temp[0].split(".")[1].find("driver") > -1:
+                        pass
+                    elif temp[0].split(".")[1].find("hostname") > -1:
+                        url = temp[1]
+                        hostname = temp[1].split("/", 2)[2].split(":")[0]
+                        port = temp[1].split("/", 2)[2].split(":")[1].split("/")[0]
+                        db = temp[1].split("/", 2)[2].split(":")[1].split("/")[1]
                     elif temp[0].split(".")[1].find("username") > -1:
                         username = temp[1]
                     elif temp[0].split(".")[1].find("passwd") > -1:
                         passwd = temp[1]
-                        catalog = Catalog(hostname, username, passwd)
+                        catalog = Catalog(hostname, username, passwd, db)
+                        catalog.displayCatalog()
                 elif temp[0].split(".")[0].find("numnodes") > -1:
                     pass
                 elif temp[0].split(".")[0].find("node") > -1:
                     kind = "node"
-                    if temp[0].split(".")[1].find("hostname") > -1:
-                        hostname = temp[1]
+                    if temp[0].split(".")[1].find("driver") > -1:
+                        pass
+                    elif temp[0].split(".")[1].find("hostname") > -1:
+                        url = temp[1]
+                        hostname = temp[1].split("/", 2)[2].split(":")[0]
+                        port = temp[1].split("/", 2)[2].split(":")[1].split("/")[0]
+                        db = temp[1].split("/", 2)[2].split(":")[1].split("/")[1]
                     elif temp[0].split(".")[1].find("username") > -1:
                         username = temp[1]
                     elif temp[0].split(".")[1].find("passwd") > -1:
                         passwd = temp[1]
-                        nodes.append(Node(hostname, username, passwd))
+                        a = Node(hostname, username, passwd, db)
+                        a.displayNode()
+                        nodes.append(a)
+
 
     # Reads ddlfile, Remove Whitespace, Remove new lines, Parse contents on ';'
     k = open(ddlfile, "r")
     sqlcmds = list(filter(None, k.read().strip().replace("\n","").split(';')))
 
-    # Running Commands
-    for cmd in sqlcmds:
-        for n in nodes:
-            try:
-                connect = pymysql.connect(n.hostname, n.username, n.passwd, db = 'TESTDB')
-                cur = connect.cursor()
-                cur.execute(cmd)
-                connect.close()
-                print("[", nodes[0].hostname, "]: ./", ddlfile, " success.")
-            except pymysql.InternalError:
-                print("[", nodes[0].hostname, "]: ./", ddlfile, " failed.")
+    # Create catalog
+    catalog.createCatalog()
+
+    # Running Commands from ddlfile
+    # for cmd in sqlcmds:
+    #     for n in nodes:
+    #         try:
+    #             connect = pymysql.connect(n.hostname, n.username, n.passwd, n.db)
+    #             cur = connect.cursor()
+    #             cur.execute(cmd)
+    #             connect.close()
+    #             print("[", nodes[0].hostname, "]: ./", ddlfile, " success.")
+    #         except pymysql.InternalError:
+    #             print("[", nodes[0].hostname, "]: ./", ddlfile, " failed.")
+
     k.close()
 
     # # Print Catalog Info
@@ -70,20 +93,31 @@ def runDDL(argv):
 
 class Catalog:
     'Base Class for Catalog'
-    def __init__(self, hostname, username, passwd):
-        self.hostname = hostname
-        self.username = username
-        self.passwd = passwd
-    def displayCatalog(self):
-        print(" Hostname: ", self.hostname, " Username: ", self.username, " Passwd: ", self.passwd)
-
-class Node:
-    'Base Class for Nodes'
-    def __init__(self, hostname, username, passwd):
+    def __init__(self, hostname, username, passwd, db):
         self.hostname = hostname.replace(" ", "")
         self.username = username.replace(" ", "")
         self.passwd = passwd.replace(" ", "")
+        self.db = db.replace(" ", "")
+    def displayCatalog(self):
+        print("Hostname: ", self.hostname, " Username: ", self.username, " Passwd: ", self.passwd, " DB: ", self.db)
+    def createCatalog(self):
+        createCMD = "create table dtables(tname VARCHAR(32), nodedriver VARCHAR(64), nodeurl VARCHAR(128), nodeuser VARCHAR(16), nodepasswd VARCHAR(16), partmtd INT, nodeid INT, partcol VARCHAR(32), partparam1 VARCHAR(32), partparam2 VARCHAR(32))"
+        print("catalog info..........")
+        self.displayCatalog()
+        connect = pymysql.connect(self.hostname, '3306', self.username, self.passwd, self.db)
+        cur = connect.cursor()
+        cur.execute(createCMD)
+        print(cur.description)
+        print("creating catalog.....")
+
+class Node:
+    'Base Class for Nodes'
+    def __init__(self, hostname, username, passwd, db):
+        self.hostname = hostname.replace(" ", "")
+        self.username = username.replace(" ", "")
+        self.passwd = passwd.replace(" ", "")
+        self.db = db.replace(" ", "")
     def displayNode(self):
-        print("Hostname: ", self.hostname, " Username: ", self.username, " Passwd: ", self.passwd)
+        print("Hostname: ", self.hostname, " Username: ", self.username, " Passwd: ", self.passwd, " DB: ", self.db)
 
 runDDL(argv)
